728bf30070a81161b1bd8bc458865be1752efe48
ElonGates <59313695+VilotStar@users.noreply.github.com>
2023-07-01 01:07:18 +0100

    too much time spent on getting the 64 bit magic, but it works, also doubled newtons approximation

--- benches/sqrt.rs
+++ benches/sqrt.rs
@@ -1,11 +1,19 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 use fastsqrt::*;
 
 fn bench_invs_sqrts(c: &mut Criterion) {
     let mut group = c.benchmark_group("invssqrt");
-    group.bench_function("fast inv sqrt 25", |b| b.iter(|| fast_inv_sqrt(black_box(25.0))));
+    /*group.bench_function("fast inv sqrt 25", |b| b.iter(|| fast_inv_sqrt(black_box(25.0))));
     group.bench_function("internal inv sqrt 25", |b| b.iter(|| normal_inv_sqrt(black_box(25.0))));
-    group.bench_function("norm inv sqrt 25", |b| b.iter(|| internal_inv_sqrt(black_box(25.0))));
+    group.bench_function("norm inv sqrt 25", |b| b.iter(|| internal_inv_sqrt(black_box(25.0))));*/
+    
+    for i in 20..26 {
+        group.bench_with_input(BenchmarkId::new("fast inv sqrt 32 ", i), &(i as f32), |b, i| b.iter(|| fast_inv_sqrt_32(black_box(*i))));
+        group.bench_with_input(BenchmarkId::new("fast inv sqrt 64 ", i), &(i as f64), |b, i| b.iter(|| fast_inv_sqrt_64(black_box(*i))));
+        group.bench_with_input(BenchmarkId::new("internal inv sqrt ", i), &(i as f64), |b, i| b.iter(|| normal_inv_sqrt(black_box(*i))));
+        group.bench_with_input(BenchmarkId::new("normal inv sqrt ", i), &(i as f64), |b, i| b.iter(|| internal_inv_sqrt(black_box(*i))));
+    }
+
     group.finish();
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
-pub fn fast_inv_sqrt(x: f64) -> f64 {
-    let mut bits: u64 = x.to_bits();
+pub fn fast_inv_sqrt_32(x: f32) -> f32 {
+    let mut bits: u32 = x.to_bits();
 
     bits = 0x5f3759df - (bits >> 1);
 
-    let y: f64 = f64::from_bits(bits);
+    let mut y: f32 = f32::from_bits(bits);
 
+    y = y * (1.5 - ((0.5 * x) * y * y));
+    y * (1.5 - ((0.5 * x) * y * y))
+}
+
+pub fn fast_inv_sqrt_64(x: f64) -> f64 {
+    let mut bits: u64 = x.to_bits();
+
+    bits = 0x5fe6ec85e7de30da - (bits >> 1);
+
+    let mut y: f64 = f64::from_bits(bits);
+
+    y = y * (1.5 - ((0.5 * x) * y * y));
     y * (1.5 - ((0.5 * x) * y * y))
 }
 
@@ -13,5 +25,17 @@ pub fn internal_inv_sqrt(x: f64) -> f64 {
 }
 
 pub fn normal_inv_sqrt(x: f64) -> f64 {
-    1.0 / x.sqrt()
+    1f64 / x.sqrt()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn inv_sqrt() {
+        let fast = fast_inv_sqrt_64(36.0);
+        let normal = normal_inv_sqrt(36.0);
+        println!("{fast} {normal}")
+    }
 }
